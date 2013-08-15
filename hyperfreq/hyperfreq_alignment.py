481c49236cb3c08f2e32ecee4add53fde7c8af4b
Christopher Small <csmall@fhcrc.org>
2013-08-15 15:06:19 -0700

    Swapped out dict for ordered dict

--- hyperfreq/hyperfreq_alignment.py
+++ hyperfreq/hyperfreq_alignment.py
@@ -1,6 +1,7 @@
 from Bio import Align, SeqRecord, Seq
 from Bio.Align import AlignInfo
 from betarat import BetaRat
+from collections import OrderedDict
 from time import time
 
 import warnings
@@ -154,7 +155,7 @@ class HyperfreqAlignment(Align.MultipleSeqAlignment):
 
         # Construct the dict we'll be returning for this sequence and pattern (will need to add cdfs and so on
         # to it)
-        hm_data = dict(
+        hm_data = OrderedDict(
                 sequence=seq.name,
                 hm_pos=hm_pos,
                 cutoff_cdf=cutoff_cdf,
